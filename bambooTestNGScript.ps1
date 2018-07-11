4e5f165765e23e987a7d64f8a6d99f84f21e0fae
Sanjay John <35611136+sanjayjohn@users.noreply.github.com>
2018-07-11 09:30:05 -0400

    Update bambooTestNGScript.ps1

--- bambooTestNGScript.ps1
+++ bambooTestNGScript.ps1
@@ -11,7 +11,7 @@ $body = "projectID=81024&testcycle=1430468&result=$newdata"
 #Curl Request
 # If (Test-Path Alias:curl) {Remove-Item Alias:curl}
 # If (Test-Path Alias:curl) {Remove-Item Alias:curl}
-# curl -v -d $body -H "cache-control: no-cache" -H "Content-Type: application/json" POST https://pulse-us-east-1.qtestnet.com/api/v1/webhooks/8ec398ee-7bc2-41e7-8ea2-8fef64874f02
+# curl -v -d $body -H "cache-control: no-cache" POST https://pulse-us-east-1.qtestnet.com/api/v1/webhooks/8ec398ee-7bc2-41e7-8ea2-8fef64874f02
 
 #Powershell Request
 Invoke-WebRequest -Uri "https://pulse-us-east-1.qtestnet.com/api/v1/webhooks/8ec398ee-7bc2-41e7-8ea2-8fef64874f02" -Method POST -Body $body
